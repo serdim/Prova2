58c76fafebf3cdc206927ed7cb911d90ffae881f
serdim <sergio.dimartino@unina.it>
2026-06-05 12:38:22 +0200

    Rename NumeroCarta to NumeroCarte in ContoCorrenteConCarta

--- PrimoEsempioPO/src/ContoCorrenteConCarta.java
+++ PrimoEsempioPO/src/ContoCorrenteConCarta.java
@@ -5,7 +5,7 @@
 public class ContoCorrenteConCarta extends ContoCorrente {
 
 	/** Il numero della carta di credito associata al conto corrente */
-	public String NumeroCarta;
+	public String NumeroCarte;
 	/** Il saldo mensile disponibile per le spese */
 	public double SaldoMensile=1500;
 	
